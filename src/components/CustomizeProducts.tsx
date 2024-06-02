7b27728da5a80c245fd3f62e983ff91ad0986e1d
Suyog Shrestha <suyogstha317@gmail.com>
2024-06-02 22:42:42 +0545

    product & category added

--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -2,6 +2,7 @@
 
 import { products } from "@wix/stores";
 import { useEffect, useState } from "react";
+import Add from "./Add";
 
 const CustomizeProducts = ({
   productId,
@@ -49,11 +50,13 @@ const CustomizeProducts = ({
   };
 
   return (
-    <div className="flex flex-col gap-6">
+    <div className='flex flex-col gap-6'>
       {productOptions.map((option) => (
-        <div className="flex flex-col gap-4" key={option.name}>
-          <h4 className="font-medium">Choose a {option.name}</h4>
-          <ul className="flex items-center gap-3">
+        <div
+          className='flex flex-col gap-4'
+          key={option.name}>
+          <h4 className='font-medium'>Choose a {option.name}</h4>
+          <ul className='flex items-center gap-3'>
             {option.choices?.map((choice) => {
               const disabled = !isVariantInStock({
                 ...selectedOptions,
@@ -69,24 +72,23 @@ const CustomizeProducts = ({
 
               return option.name === "Color" ? (
                 <li
-                  className="w-8 h-8 rounded-full ring-1 ring-gray-300 relative"
+                  className='w-8 h-8 rounded-full ring-1 ring-gray-300 relative'
                   style={{
                     backgroundColor: choice.value,
                     cursor: disabled ? "not-allowed" : "pointer",
                   }}
                   onClick={clickHandler}
-                  key={choice.description}
-                >
+                  key={choice.description}>
                   {selected && (
-                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                    <div className='absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />
                   )}
                   {disabled && (
-                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                    <div className='absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2' />
                   )}
                 </li>
               ) : (
                 <li
-                  className="ring-1 ring-primary text-primary rounded-md py-1 px-4 text-sm"
+                  className='ring-1 ring-lama text-lama rounded-md py-1 px-4 text-sm'
                   style={{
                     cursor: disabled ? "not-allowed" : "pointer",
                     backgroundColor: selected
@@ -96,11 +98,9 @@ const CustomizeProducts = ({
                       : "white",
                     color: selected || disabled ? "white" : "#f35c7a",
                     boxShadow: disabled ? "none" : "",
-                    
                   }}
                   key={choice.description}
-                  onClick={clickHandler}
-                >
+                  onClick={clickHandler}>
                   {choice.description}
                 </li>
               );
@@ -108,3 +108,8 @@ const CustomizeProducts = ({
           </ul>
         </div>
       ))}
+    </div>
+  );
+};
+
+export default CustomizeProducts;
